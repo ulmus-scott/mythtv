ulmus-scott/mythtv

commit 738662361a47c827bc5d86bb8a14fa85065a3417

Disable high DPI scaling by default for Qt6
Disable the high DPI scaling if its not configured by the
user in environment variable QT_ENABLE_HIGHDPI_SCALING.

mythtv/libs/libmythui/mythdisplay.cpp
@@ -1264,6 +1264,12 @@ void MythDisplay::ConfigureQtGUI(int SwapInterval, const MythCommandLineParser& 
 #if QT_VERSION < QT_VERSION_CHECK(6,0,0)
     // Ignore desktop scaling
     QApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
+#else
+    // Disable high DPI scaling unless defined in the environment
+if (qEnvironmentVariableIsEmpty("QT_ENABLE_HIGHDPI_SCALING"))
+    {
+        qputenv("QT_ENABLE_HIGHDPI_SCALING", "0");
+    }
 #endif
 
 #if CONFIG_X11

commit b437671f434fc778f610f42ae4de0cf93393ad63

Fix layout inconsistency
Fix indentation error introduced in commit 738662361a47c827bc5d86bb8a14fa85065a3417

mythtv/libs/libmythui/mythdisplay.cpp
@@ -1266,7 +1266,7 @@ void MythDisplay::ConfigureQtGUI(int SwapInterval, const MythCommandLineParser& 
     QApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
 #else
     // Disable high DPI scaling unless defined in the environment
-if (qEnvironmentVariableIsEmpty("QT_ENABLE_HIGHDPI_SCALING"))
+    if (qEnvironmentVariableIsEmpty("QT_ENABLE_HIGHDPI_SCALING"))
     {
         qputenv("QT_ENABLE_HIGHDPI_SCALING", "0");
     }