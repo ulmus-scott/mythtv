cc1ef6f1b62833971a5a6dbc0191c8afb4ef1ec0
Peter Bennett <pbennett@mythtv.org>
2026-04-09 11:07:53 -0400

    frontend web app: Support old URL for remote
    In case somebody has it bookmarked.

--- mythtv/html/frontend/src/app/app.routes.ts
+++ mythtv/html/frontend/src/app/app.routes.ts
@@ -5,4 +5,6 @@ import { Remote } from './remote/remote';
 export const routes: Routes = [
     { path: '', component: Status },
     { path: 'remote', component: Remote },
+    // Old path in case somebody has it bookmarked
+    { path: 'MythFE/GetRemote', component: Remote },
 ];
